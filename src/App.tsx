e4af861e5872f8abeff667be6a71026c430c0306
SumiT0998 <sumit.bisht36@gmail.com>
2024-07-13 20:48:42 +0530

    update homepage

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import { Signin } from "./pages/signin";
 import { Blogs } from "./pages/Blogs";
 import { Blog } from "./pages/Blog";
 import { Publish } from "./pages/Publish";
+import { Home } from "./pages/Home";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/blog/:id" element={<Blog />} />
@@ -20,7 +22,5 @@ function App() {
     </>
   );
 }
-
-
 
 export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import { Appbar } from "../componenets/Appbar";
+
+export const Home = () => {
+  return (
+    <div>
+      <div>
+        <Appbar />
+        <div className="flex justify-center">home</div>
+      </div>
+    </div>
+  );
+};
